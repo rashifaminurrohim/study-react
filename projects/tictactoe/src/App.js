e5eff225d3461c016a35c04d6962875793463542
rashifaminurrohim <muhammad.rashif@mhs.itenas.ac.id>
2026-03-18 00:31:33 +0700

    tic tac toe:  replace hardcoded row/col arrays using loop

--- projects/tictactoe/src/App.js
+++ projects/tictactoe/src/App.js
@@ -30,24 +30,22 @@ function Board({ xIsNext, squares, onPlay }) {
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
+  const row = [0, 1, 2];
+  const col = [0, 1, 2];
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {row.map((_, r) => (
+        <div className="board-row" key={r}>
+          {col.map((_, c) => (
+            <Square
+              key={c}
+              value={squares[r * 3 + c]}
+              onSquareClick={() => handleClick(r * 3 + c)}
+            />
+          ))}
+        </div>
+      ))}
     </>
   );
 }
