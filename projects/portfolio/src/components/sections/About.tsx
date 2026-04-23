f3d2b592917237176449fcb8273b6a8c7af49288
rashifaminurrohim <muhammad.rashif@mhs.itenas.ac.id>
2026-04-23 13:22:51 +0700

    portfolio: make about section responsive

--- projects/portfolio/src/components/sections/About.tsx
+++ projects/portfolio/src/components/sections/About.tsx
@@ -1,8 +1,35 @@
 import { LiaDownloadSolid } from "react-icons/lia";
 
 export default function About() {
+  const experiences = [
+    {
+      role: "Cloud Computing Trainee",
+      place: "Orbit Future Academy",
+      desc: "Learned cloud fundamentals and deployed applications using core cloud services.",
+      period: "Agt — Oct 2025",
+    },
+    {
+      role: "Fullstack Web Development Cohort",
+      place: "DBS Coding Camp 2025",
+      desc: "Built fullstack web applications using HTML, CSS, JS, React and Hapijs.",
+      period: "Feb — Jul 2025",
+    },
+    {
+      role: "Operations Assistant Intern",
+      place: "PT Taspen Persero Bandung",
+      desc: "Performed document digitization, assisted in claims processing, and managed archival records.",
+      period: "Oct — Feb 2025",
+    },
+    {
+      role: "Android Development Cohort",
+      place: "Bangkit Academy 2024",
+      desc: "Built Android applications using Kotlin, MVVM, and clean architecture.",
+      period: "Feb — Jul 2024",
+    },
+  ];
+
   return (
-    <div
+    <section
       className="
         relative
         py-24 -mb-24
@@ -13,24 +40,26 @@ export default function About() {
       <div className="max-w-6xl mx-auto px-6">
         <div className="grid md:grid-cols-2 gap-12 items-start ">
           {/* LEFT — About */}
-          <div className="flex flex-col items-center justify-evenly">
-            <h2 className="text-heading mb-6 text-center">About Me</h2>
+          <section className="flex flex-col items-center justify-evenly">
+            <h2 className="text-heading mb-6">About Me</h2>
 
-            <p className="text-paragraph mb-4">
-              Hi! I'm Rashif, a software developer with a strong interest in
-              Mobile and Web development. I enjoy building clean, scalable
-              applications and turning ideas into structured, user-friendly
-              solutions. I'm currently focused on improving my skills in modern
-              web technologies while continuing to deepen my experience in
-              mobile development. I'm particularly interested in clean
-              architecture, maintainable code, and creating interfaces that feel
-              simple and intuitive.
-            </p>
-
-            <p className="text-paragraph">
-              Outside of coding, I enjoy working out, watching movies, and
-              continuously learning things that help me grow as a developer.
-            </p>
+            <div className="mx-3 text-paragraph text-zinc-600">
+              <p>
+                Hi! I'm Rashif, a software developer with a strong interest in
+                Mobile and Web development. I enjoy building clean, scalable
+                applications and turning ideas into structured, user-friendly
+                solutions. I'm currently focused on improving my skills in
+                modern web technologies while continuing to deepen my experience
+                in mobile development. I'm particularly interested in clean
+                architecture, maintainable code, and creating interfaces that
+                feel simple and intuitive.
+              </p>
+              <br />
+              <p>
+                Outside of coding, I enjoy working out, watching movies, and
+                continuously learning things that help me grow as a developer.
+              </p>
+            </div>
 
             <a
               href="/cv-muhammad-rashif-a.pdf"
@@ -42,82 +71,27 @@ export default function About() {
                 <LiaDownloadSolid className="text-primary w-4 h-4" />
               </div>
             </a>
-          </div>
+          </section>
 
           {/* RIGHT — Experience */}
-          <div className="flex flex-col items-center">
+          <section className="flex flex-col items-center">
             <h3 className="text-heading mb-6 text-center">Experience</h3>
 
-            <div className="space-y-6 max-w-md w-full">
-              <div className="grid grid-cols-12 gap-4">
-                <div className="col-span-4 text-paragraph whitespace-nowrap">
-                  <p>Agt — Oct 2025</p>
+            {experiences.map((experience) => (
+              <div className="grid lg:grid-cols-12 max-w-md mx-3 gap-1 lg:gap-4">
+                <div className="lg:col-span-4 text-paragraph whitespace-nowrap">
+                  <p>{experience.period}</p>
                 </div>
-                <div className="col-span-8">
-                  <p className="font-medium">Cloud Computing Cohort</p>
-                  <p className="text-sm text-zinc-500">Orbit Future Academy</p>
-                  <p className="text-sm text-zinc-600 mt-2">
-                    Built Android applications using Kotlin, MVVM, and clean
-                    architecture.
-                  </p>
+                <div className="lg:col-span-8 mb-4">
+                  <p className="font-medium text-accent">{experience.role}</p>
+                  <p className="text-sm text-zinc-400 mb-1">{experience.place}</p>
+                  <p className="text-sm text-zinc-600">{experience.desc}</p>
                 </div>
               </div>
-            </div>
-            <br />
-
-            <div className="space-y-6 max-w-md w-full">
-              <div className="grid grid-cols-12 gap-4">
-                <div className="col-span-4 text-paragraph whitespace-nowrap">
-                  <p>Feb — Jul 2025</p>
-                </div>
-                <div className="col-span-8">
-                  <p className="font-medium">
-                    Fullstack Web Development Cohort
-                  </p>
-                  <p className="text-sm text-zinc-500">DBS Coding Camp</p>
-                  <p className="text-sm text-zinc-600 mt-2">
-                    Built Android applications using Kotlin, MVVM, and clean
-                    architecture.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <br />
-
-            <div className="space-y-6 max-w-md w-full">
-              <div className="grid grid-cols-12 gap-4">
-                <div className="col-span-4 text-paragraph whitespace-nowrap">
-                  <p>Oct — Feb 2025</p>
-                </div>
-                <div className="col-span-8">
-                  <p className="font-medium">Intern</p>
-                  <p className="text-sm text-zinc-500">Taspen Bandung</p>
-                  <p className="text-sm text-zinc-600 mt-2">
-                    Scanning, Managing Claims.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <br />
-
-            <div className="space-y-6 max-w-md w-full">
-              <div className="grid grid-cols-12 gap-4">
-                <div className="col-span-4 text-paragraph whitespace-nowrap">
-                  <p>Feb — Jul 2024</p>
-                </div>
-                <div className="col-span-8">
-                  <p className="font-medium">Android Development Cohort</p>
-                  <p className="text-sm text-zinc-500">Bangkit Academy</p>
-                  <p className="text-sm text-zinc-600 mt-2">
-                    Built Android applications using Kotlin, MVVM, and clean
-                    architecture.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+            ))}
+          </section>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
