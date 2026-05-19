205d626e7590471197e9cdf4e6eb694a822cf4a4
rashifaminurrohim <muhammad.rashif@mhs.itenas.ac.id>
2026-05-19 10:46:10 +0700

    React101: latihan 2 virtual cardname dinamis wrapped

--- codepolitan/vcardname/src/App.jsx
+++ codepolitan/vcardname/src/App.jsx
@@ -1,61 +1,129 @@
 import "./vcard-styles.css";
 import { Icon } from "@iconify/react";
 
+const socials = [
+  {
+    logo: "mdi:facebook",
+    link: "https://www.instagram.com/rasifara_/",
+  },
+  {
+    logo: "mdi:instagram",
+    link: "https://www.instagram.com/rasifara_/",
+  },
+  {
+    logo: "mdi:twitter",
+    link: "https://www.instagram.com/rasifara_/",
+  },
+  {
+    logo: "mdi:linkedin",
+    link: "https://www.instagram.com/rasifara_/",
+  },
+  {
+    logo: "mdi:github",
+    link: "https://www.instagram.com/rasifara_/",
+  },
+];
+
+const badges = [
+  {
+    badge: "twemoji:microscope",
+    text: "Phd",
+  },
+  {
+    badge: "fluent-emoji-flat:mountain",
+    text: "mountaineer",
+  },
+  {
+    badge: "game-icons:rolling-dices",
+    text: "3D Geek",
+  },
+];
+
+function SocialButton({ logo, link }) {
+  return (
+    <button onClick={() => window.open(link, "_blank")}>
+      <Icon icon={logo} />
+    </button>
+  );
+}
+
+function Identity() {
+  return (
+    <div className="text">
+      <div className="name-wrapper">
+        <div className="name">Black Cat</div>
+      </div>
+      <div className="title">Meow Engineer</div>
+    </div>
+  );
+}
+
+function Header() {
+  const middleIndex = Math.round(socials.length / 2);
+  const socialsLeft = socials.slice(0, middleIndex);
+  const socialsRight = socials.slice(middleIndex);
+  return (
+    <>
+      <div className="social-buttons">
+        {socialsLeft.map((s) => (
+          <SocialButton logo={s.logo} link={s.link} />
+        ))}
+      </div>
+      <div className="social-buttons right">
+        {socialsRight.map((s) => (
+          <SocialButton logo={s.logo} link={s.link} />
+        ))}
+      </div>
+    </>
+  );
+}
+
+function Biodata() {
+  return (
+    <div className="desc">
+      <p>
+        My Name is <b>Black cat</b> and I am a Developer.
+      </p>
+      <p>
+        To say that I love software is understatement, I spend every waking hour
+        building something FUN & watching Tech Podcast or attending Meetups &
+        Conferences.
+      </p>
+    </div>
+  );
+}
+
+function Badge({ badge, text }) {
+  return (
+    <button>
+      <div className="height">
+        <Icon icon={badge} height={12} style={{ marginRight: "2px" }} />
+        {text}
+      </div>
+    </button>
+  );
+}
+
+function Highlight() {
+  return (
+    <div className="buttons">
+      {badges.map((b) => (
+        <Badge badge={b.badge} text={b.text} />
+      ))}
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="card">
       <div className="top">
-        <div className="social-buttons">
-          <button>
-            <Icon icon={"mdi:facebook"} />
-          </button>
-          <button>
-            <Icon icon={"mdi:instagram"} />
-          </button>
-          <button>
-            <Icon icon={"mdi:twitter"} />
-          </button>
-        </div>
-        <div className="social-buttons right">
-          <button>
-            <Icon icon={"mdi:linkedin"} />
-          </button>
-          <button>
-            <Icon icon={"mdi:github"} />
-          </button>
-        </div>
-        <button className="like">
-          <Icon icon={"mdi:heart"} />
-        </button>
-        <div className="text">
-          <div className="name-wrapper">
-            <div className="name">Black Cat</div>
-          </div>
-          <div className="title">Meow Engineer</div>
-        </div>
+        <Header />
+        <Identity />
       </div>
       <div className="bottom">
-        <div className="desc">
-          <p>My Name is <b>Black cat</b> and I am a Developer.</p>
-          <p>To say that I love software is understatement, I spend every waking hour building something FUN & watching Tech Podcast or attending Meetups & Conferences.</p>
-          <div className="buttons height">
-            <button>
-              <Icon icon={"twemoji:microscope"} />
-              <p>Phd</p>
-            </button>
-            <button>
-              <Icon icon={"fluent-emoji-flat:mountain"} />
-              <p>mountaineer</p>
-            </button>
-            <button>
-              <Icon
-                icon={"game-icons:rolling-dices"}
-                style={{ color: "white" }}
-              />
-              <p>3D Geek</p>
-            </button>
-          </div>
-        </div>
+        <Biodata />
+        <Highlight />
       </div>
     </div>
   );
