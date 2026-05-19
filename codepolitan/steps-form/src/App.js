a03061a430e5d832100e6825a56a273f786f2288
rashifaminurrohim <muhammad.rashif@mhs.itenas.ac.id>
2026-05-19 14:57:34 +0700

    React state: project steps form

---
+++ codepolitan/steps-form/src/App.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+const stepItems = ['Dream', 'Believe', 'Achieve'];
+
+function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handleNext() {
+    if (step < 3) {
+      setStep((step) => step + 1);
+      console.log(step);
+    }
+  }
+
+  function handlePrev() {
+    if (step > 1) {
+      setStep((step) => step - 1);
+      console.log(step);
+    }
+  }
+
+  return (
+    <>
+      <button className="close" onClick={() => setIsOpen((isOpen) => !isOpen)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className='steps'>
+
+          <div className='numbers'>
+            <div className={step === 1 ? 'active' : ''}>1</div>
+            <div className={step === 2 ? 'active' : ''}>2</div>
+            <div className={step === 3 ? 'active' : ''}>3</div>
+          </div>
+          <p className='message'>Step {step} : {stepItems[step - 1]}</p>
+          <div className='buttons'>
+            <button
+              style={{ backgroundColor: "#526d82", color: "#fff" }}
+              onClick={handlePrev}
+            >Prev</button>
+            <button
+              style={{ backgroundColor: "#526d82", color: "#fff" }}
+              onClick={handleNext}
+            >Next</button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default App;
