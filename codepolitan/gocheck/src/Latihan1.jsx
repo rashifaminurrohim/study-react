2b0487ff3eb898552ced2ccf29492ea168ba7dd1
rashifaminurrohim <muhammad.rashif@mhs.itenas.ac.id>
2026-06-01 21:03:18 +0700

    React State: Jawaban Latihan 1

--- codepolitan/gocheck/src/Latihan1.jsx
+++ codepolitan/gocheck/src/Latihan1.jsx
@@ -1,76 +1,57 @@
 import { useState } from "react";
 
 export default function Latihan1() {
-  const [tasks, setTasks] = useState([]);
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      text: "hello",
+    },
+  ]);
 
-  function handleAddTask(task) {
-    setTasks((prev) => [
-      ...prev,
-      { id: Date.now(), task: task, status: false },
-    ]);
+  const [inputNote, setInputNote] = useState("");
+
+  function handleInputChange(e) {
+    setInputNote(e.target.value);
   }
 
-  function handleDeleteTask(id) {
-    console.log(id);
-    if (id) {
-      setTasks((prev) => prev.filter((task) => task.id !== id));
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!inputNote.trim() !== "") {
+      const newNote = {
+        id: new Date().getTime(),
+        text: inputNote,
+      };
+
+      setNotes([...notes, newNote]);
+      setInputNote("");
     }
+  }
+
+  function handleDeleteNote(id) {
+    const updatedNotes = notes.filter((note) => note.id !== id);
+    setNotes(updatedNotes);
   }
 
   return (
     <div className="App">
       <h1>Notes</h1>
-      <AddTask addtask={handleAddTask} />
-      {tasks.map((t) => (
-        <Tasklist
-          key={t.id}
-          id={t.id}
-          task={t.task}
-          status={t.status}
-          deleteTask={handleDeleteTask}
+      <form className="note-input" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Add a note"
+          value={inputNote}
+          onChange={handleInputChange}
         />
-      ))}
-    </div>
-  );
-}
-
-function AddTask({ addtask }) {
-  const [task, setTask] = useState("");
-
-  function handleSubmit() {
-    if (!task.trim()) return;
-    addtask(task);
-    setTask("");
-  }
-
-  return (
-    <form onSubmit={(e) => e.preventDefault()}>
-      <input
-        type="text"
-        name="task"
-        id="task"
-        value={task}
-        onChange={(e) => setTask(e.target.value)}
-      />
-      <button onClick={handleSubmit}>Add</button>
-    </form>
-  );
-}
-
-function Tasklist({ id, task, status, deleteTask }) {
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        backgroundColor: "gray",
-        margin: "1rem",
-        padding: "0.5rem",
-        justifyContent: "space-between",
-      }}
-    >
-      <p>{task}</p>
-      <button onClick={() => deleteTask(id)}>Delete</button>
+        <button>Add</button>
+      </form>
+      <ul className="note-list">
+        {notes.map((note) => (
+          <li key={note.id}>
+            {note.text}
+            <button onClick={() => handleDeleteNote(note.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
--- codepolitan/gocheck/src/index.js
+++ codepolitan/gocheck/src/index.js
@@ -4,15 +4,15 @@ import ReactDOM from 'react-dom/client';
 import './latihan_1_2.css';
 // import App from './App';
 // import Notes from './Notes';
-// import Latihan1 from './Latihan1';
-import Latihan2 from './Latihan2';
+import Latihan1 from './Latihan1';
+// import Latihan2 from './Latihan2';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <Notes /> */}
-    {/* <Latihan1 /> */}
-    <Latihan2 />
+    <Latihan1 />
+    {/* <Latihan2 /> */}
   </React.StrictMode>
 );
 
